6bbfb45a4e989397b8fe44f607921e2fee266613
Alex Koppel <75754097+Ezayji@users.noreply.github.com>
2020-12-21 11:58:29 +0200

    Add hover effect with css instead

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 // Navigation items
 
 let mission = document.getElementById("mis");
@@ -7,28 +9,26 @@ let contact = document.getElementById("con");
 
 // Function for border / shadow
 
-function addBorder(event){
-/*    event.target.style.border = "2px solid #e7e7e7";
-    event.target.style.borderRadius = "10px";
-*/
+function addShadow(event){
     event.target.style.textShadow = "0 0 40px #982dfd";
 }
 
-function removeBorder(event){
-//    event.target.style.border = "none";
+function removeShadow(event){
     event.target.style.textShadow = "0 6px 13px black";
 }
 
 // Targets for Shadows on mouseover
 
-mission.onmouseover = addBorder;
-mission.onmouseout = removeBorder;
+mission.onmouseover = addShadow;
+mission.onmouseout = removeShadow;
 
-artists.onmouseover = addBorder;
-artists.onmouseout = removeBorder;
+artists.onmouseover = addShadow;
+artists.onmouseout = removeShadow;
 
-portfolio.onmouseover = addBorder;
-portfolio.onmouseout = removeBorder;
+portfolio.onmouseover = addShadow;
+portfolio.onmouseout = removeShadow;
 
-contact.onmouseover = addBorder;
-contact.onmouseout = removeBorder;
+contact.onmouseover = addShadow;
+contact.onmouseout = removeShadow;
+
+*/
